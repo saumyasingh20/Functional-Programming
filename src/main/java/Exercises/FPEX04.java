ed2579da9c3a01aa6a6f85cea3d6de6cf374c1d7
saumyasingh20 <saumyaaaa98@gmail.com>
2022-09-02 13:04:27 +0530

    day 2 intro to map method

--- src/main/java/Exercises/FPEX04.java
+++ src/main/java/Exercises/FPEX04.java
@@ -6,13 +6,21 @@ public class FPEX04 {
     public static void main(String[] args) {
 
         List<String> courses = List.of("Spring","Spring Boot","API","Microservices","AWS","PCF","Azure","Docker","Kubernetes");
-        printCoursesWithAtLeastFourLetters(courses);
+        //printCoursesWithAtLeastFourLetters(courses);
+        printNumOfCharacters(courses);
     }
 
     private static void printCoursesWithAtLeastFourLetters(List<String> courses) {
 
         courses.stream()
                 .filter(course -> course.length()>=4)
+                .forEach(System.out::println);
+    }
+
+    private static void printNumOfCharacters(List<String> courses) {
+
+        courses.stream()
+                .map(course -> course + " has " + course.length() + " characters")
                 .forEach(System.out::println);
     }
 }
--- src/main/java/Programming/FP01Functional.java
+++ src/main/java/Programming/FP01Functional.java
@@ -6,7 +6,23 @@ public class FP01Functional {
     public static void main(String[] args) {
 
         //printAllNumbersFunctional(List.of(12,98,98,9,87,23,56,45,65));
-        printEvenNumbersFunctional(List.of(12,98,98,9,87,23,56,45,65));
+        //printEvenNumbersFunctional(List.of(12,98,98,9,87,23,56,45,65));
+        printSquaresOfEvenNumbersFunctional(List.of(12,98,98,9,87,23,56,45,65));
+      //  printCubesOfOddNumbersFunctional(List.of(12,98,98,9,87,23,56,45,65));
+    }
+
+    private static void printSquaresOfEvenNumbersFunctional(List<Integer> integers) {
+        integers.stream()
+                .filter(num -> num%2==0)
+                .map(num -> num + " " + num*num)
+                .forEach(System.out::println);
+    }
+
+    private static void printCubesOfOddNumbersFunctional(List<Integer> integers) {
+        integers.stream()
+                .filter(num -> num%2==1)
+                .map(num -> num + " " + num*num*num)
+                .forEach(System.out::println);
     }
 
     private static boolean isEven(int num){
